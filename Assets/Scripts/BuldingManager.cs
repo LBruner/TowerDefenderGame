75d7b31068a7a75624d3888703a3ec414f31cff8
LBruner <leandrobruner15@gmail.com>
2021-12-01 15:02:53 -0300

    start building placement system (06)

---
+++ Assets/Scripts/BuldingManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuldingManager : MonoBehaviour
+{
+    [SerializeField] private Transform pfWoodHarvester;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWouldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWouldPosition.z = 0f;
+        return mouseWouldPosition;
+    }
+}
